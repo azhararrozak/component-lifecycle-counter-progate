0c00b346ab0ad3675dc5908a1871c42af50d093d
azhararrozak <azhardrozak2001@upi.edu>
2024-06-06 19:36:32 +0700

    useEffect explanation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react'
+import Counter from './components/Counter'
+import GameOver from './components/GameOver'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [isGameOver, setIsGameOver] = useState(false)
+
+  useEffect(() => {
+    console.log('rendered')
+    return () => {
+      console.log('cleaned up')
+    }
+  })
+  
+  const finishGame = () => {
+    setIsGameOver(true)
+  }
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={styles.container}>
+      {isGameOver ?
+        <GameOver /> : <Counter finishGame={finishGame} />
+      }
+    </div>
   )
+}
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    marginTop: '30px,',
+  },
 }
 
 export default App
